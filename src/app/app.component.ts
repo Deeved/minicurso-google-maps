71457f2c9c0f01c97acd02780337f70003e2ce09
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2023-06-28 17:37:39 -0300

    feat: adding map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'minicurso-google-maps';
+export class AppComponent implements OnInit {
+  // https://developers.google.com/maps/documentation/javascript/using-typescript?hl=pt-br
+
+  title = "minicurso-google-maps";
+  map: google.maps.Map;
+  center: google.maps.LatLngLiteral = { lat: -7.179322, lng: -39.2796439 };
+  marker: google.maps.Marker;
+
+  ngOnInit(): void {
+    this.loadMap();
+  }
+
+  loadMap(): void {
+    this.map = new google.maps.Map(
+      document.getElementById("map") as HTMLElement,
+      {
+        center: this.center,
+        zoom: 12,
+      }
+    );
+  }
 }
