119fe63d51c1f86a1de157627ceb3f0a1ac80c2f
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2023-07-04 16:25:24 -0300

    chore: add @brisanet/ion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { HttpClientModule } from "@angular/common/http";
 import { BasicComponent } from "./basic/basic.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { MarkersComponent } from "./markers/markers.component";
-import { PolylinesComponent } from './polylines/polylines.component';
+import { PolylinesComponent } from "./polylines/polylines.component";
+import { IonButtonModule } from "@brisanet/ion";
 @NgModule({
-  declarations: [AppComponent, BasicComponent, MarkersComponent, PolylinesComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    BasicComponent,
+    MarkersComponent,
+    PolylinesComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, IonButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
