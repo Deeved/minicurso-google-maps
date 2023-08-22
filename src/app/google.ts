36ef34f6b6ba27ca3a5d3960edd7161941b49714
Deeved Hiuston <deeved@gmail.com>
2023-08-22 19:44:45 -0300

    refactor: update google.ts

--- src/app/google.ts
+++ src/app/google.ts
@@ -1,82 +1,61 @@
 /// <reference types="@types/google.maps" />
-interface Settings {
-  [x: string]: any;
-}
-
-export interface Marker<T = {}> extends google.maps.Marker {
-  item?: T;
-}
 
 interface MapSettings {
   element: HTMLDivElement;
-  map: any;
-  initialSettings: Settings;
-}
-
-export interface MapSettingsPromise extends MapSettings {
-  maps: any;
+  map: google.maps.Map | undefined;
+  initialSettings: Record<string, any>;
 }
 
 const instances: Map<string, MapSettings> = new Map();
 
-export function MapInstance(
+export function createMapInstance(
   alias: string,
   id: string,
-  settings: Settings = {},
-  width: number | string = "100%",
-  height: number | string = "100%"
-): Promise<MapSettingsPromise> {
+  settings: Record<string, any> = {},
+  width: string | number = "100%",
+  height: string | number = "100%"
+): Promise<MapSettings> {
   return new Promise((resolve, reject) => {
-    if ((window as any).google) {
-      const elementWrapper = document.getElementById(id);
-      if (elementWrapper) {
-        elementWrapper.childNodes.forEach((item) => item.remove());
-        const element: HTMLDivElement = document.createElement("div");
-        if (element.hasAttribute("style")) {
-          element.style.width = `${
-            typeof width === "string" ? width : width + "px"
-          }`;
-          element.style.height = `${
-            typeof height === "string" ? height : height + "px"
-          }`;
-        } else {
-          element.setAttribute(
-            "style",
-            `width: ${
-              typeof width === "string" ? width : width + "px"
-            };height: ${typeof height === "string" ? height : height + "px"};`
-          );
-        }
-        const instance: MapSettings | undefined = instances.get(alias);
-        if (instance) {
-          elementWrapper.append(instance.element);
-          (instance as MapSettingsPromise).maps = (window as any).google.maps;
-          resolve(instance as MapSettingsPromise);
-        } else {
-          const map = new (window as any).google.maps.Map(element, {
-            center: { lat: -34.397, lng: 150.644 },
-            zoom: 8,
-            ...settings,
-          });
-          elementWrapper.append(element);
-          instances.set(alias, {
-            element,
-            map,
-            initialSettings: settings,
-          });
-
-          const instanceMapSettings: MapSettings | undefined =
-            instances.get(alias);
-          if (instanceMapSettings) {
-            (instanceMapSettings as MapSettingsPromise).maps = (
-              window as any
-            ).google.maps;
-            resolve(instanceMapSettings as MapSettingsPromise);
-          }
-        }
-      } else {
-        reject({ msg: "Element wrapper is not found" });
-      }
+    const googleMaps = (window as any).google;
+    if (!googleMaps) {
+      reject({ msg: "Google Maps is not loaded" });
+      return;
     }
+
+    const elementWrapper = document.getElementById(id);
+
+    if (!elementWrapper) {
+      reject({ msg: "Element wrapper not found" });
+      return;
+    }
+
+    elementWrapper.innerHTML = "";
+
+    const element = document.createElement("div");
+    element.style.width = typeof width === "string" ? width : width + "px";
+    element.style.height = typeof height === "string" ? height : height + "px";
+
+    const existingInstance = instances.get(alias);
+    if (existingInstance) {
+      elementWrapper.appendChild(existingInstance.element);
+      resolve(existingInstance);
+      return;
+    }
+
+    const map = new google.maps.Map(element, {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+      ...settings,
+    });
+
+    const newInstance: MapSettings = {
+      element,
+      map,
+      initialSettings: settings,
+    };
+
+    instances.set(alias, newInstance);
+    elementWrapper.appendChild(newInstance.element);
+    resolve(newInstance);
   });
 }
